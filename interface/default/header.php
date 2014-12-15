2b00c2ab4ff6e4830315c7c26e906f94919a8e94
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2014-12-15 11:44:35 +0200

    includ ai-plugin.js

--- interface/default/header.php
+++ interface/default/header.php
@@ -42,6 +42,8 @@
     <script src="<?=$project->assets_url?>bootstrap/js/bootstrap.min.js"></script>
 <!--	<script type="text/javascript" src="https://www.google.com/jsapi"></script>  -->
 
+	<script src="<?=$project->assets_url?>js/ai-plugin.js"></script>
+
 	<script src="<?=$project->assets_url?>js/dropdown-menu.js"></script>
 	<script src="<?=$project->assets_url?>js/ajax-crud.js"></script>
 	<script src="<?=$project->assets_url?>js/datepicker.js"></script>
